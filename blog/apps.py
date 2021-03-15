adde86f16be575f8663a957ab4822656d107437c
winsbar <slavaku2014@gmail.com>
2021-03-15 11:29:59 +0200

    - Created Models (Profile and Post) - Added Signals (to create Profiles parallel with new User)

--- blog/apps.py
+++ blog/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     name = 'blog'
+
+
+class ProfilesConfig(AppConfig):
+    name = 'profiles'
+
+    def ready(self):
+        import blog.signals
+
+
