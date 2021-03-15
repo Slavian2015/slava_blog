b277049bc1e30b0a1ebc6057f49c0501585fa654
winsbar <slavaku2014@gmail.com>
2021-03-15 12:11:04 +0200

    - Added HTML pages

--- blog/apps.py
+++ blog/apps.py
@@ -8,7 +8,6 @@ class BlogConfig(AppConfig):
 class ProfilesConfig(AppConfig):
     name = 'profiles'
 
-    def ready(self):
-        import blog.signals
+
 
 
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 from .models import Post, Profile
 
 
+def follow_unfollow(my_id, id_to_follow):
+    profile = my_id
+    pk = id_to_follow
+    obj = Profile.objects.get(pk=pk)
+
+    if obj.user in profile.following.all():
+        print("remove :", "\n", obj.user, "\n", profile.following.all())
+        profile.following.remove(obj.user)
+    else:
+        print("add :", "\n", obj.user, "\n", profile.following.all())
+        profile.following.add(obj.user)
+    return
+
+
 def posts_of_following_profiles(request):
     profile = Profile.objects.get(user=request.user)
 
@@ -15,6 +29,10 @@ def posts_of_following_profiles(request):
 
     my_posts = profile.profiles_posts()
     posts.append(my_posts)
+
+    """ This command for adding User to follow/unfollow 
+    (it should be turned on only once and turned off immediately after first page refresh) """
+    # follow_unfollow(profile, 2)
 
     return render(request, 'home.html', {"profile": profile, "posts": posts})
 
